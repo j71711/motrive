e2c49d0d52bc9299dc55c9c4e08ddfa1bdc5c06b
Jalnar alharthi <jalnar@Jalnars-MacBook-Pro.local>
2026-05-19 10:19:14 +0300

    local data

--- motrive/ios/Runner/AppDelegate.swift
+++ motrive/ios/Runner/AppDelegate.swift
@@ -9,6 +9,8 @@ import flutter_local_notifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     GMSServices.provideAPIKey(" AIzaSyBt4gNdEkOQmv4--y9_f2_UgwAmTheTjdY")
+
     UNUserNotificationCenter.current().delegate = self
 
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
